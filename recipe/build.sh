ca94cf5627dbb79a725e5c8fa61b0f8a0861f0ce
Ray Donnelly <mingw.android@gmail.com>
2017-09-13 12:43:07 +0100

    (WIP) Disable openblas, add cross-compilation support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
 
-if [ "$(uname)" == "Darwin" ]
-then
-    rm "$PREFIX"/lib/libgslcblas.*
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.dylib"
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.0.dylib"
-elif [ "$(uname)" == "Linux" ]
-then
-    rm "$PREFIX"/lib/libgslcblas.*
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so"
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so.0"
-fi
-
+# if [[ ${HOST} =~ .*darwin.* ]]; then
+#     rm "$PREFIX"/lib/libgslcblas.*
+#     ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.dylib"
+#     ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.0.dylib"
+# elif [[ ${HOST} =~ .*linux.* ]]; then
+#     rm "$PREFIX"/lib/libgslcblas.*
+#     ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so"
+#     ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so.0"
+# fi
